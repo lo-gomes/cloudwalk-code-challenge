a1cda5a35ec19105b3bf5c9f5ed1accdfdf70c51
Lucas Oliveira <loliveiraold@gmail.com>
2022-04-28 12:32:26 -0300

    Moves config constants to spec_helper file so that the other test can use it as well.

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,10 +1,5 @@
 # frozen_string_literal: true
 
-SAMPLE_LOG = File.join(File.dirname(__FILE__), "stubs", "sample_log.log")
-SIMPLE_KILL_LOG = File.join(File.dirname(__FILE__), "stubs", "simple_kill_log.log")
-WORLD_KILL_LOG = File.join(File.dirname(__FILE__), "stubs", "world_kill_log.log")
-KILLS_BY_MEANS_LOG = File.join(File.dirname(__FILE__), "stubs", "kills_by_means_log.log")
-
 describe Parser do
   describe "#parse" do
     it "should raise an error when log file is not found" do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,11 @@
 
 require "active_support/all"
 
+SAMPLE_LOG = File.join(File.dirname(__FILE__), "stubs", "sample_log.log")
+SIMPLE_KILL_LOG = File.join(File.dirname(__FILE__), "stubs", "simple_kill_log.log")
+WORLD_KILL_LOG = File.join(File.dirname(__FILE__), "stubs", "world_kill_log.log")
+KILLS_BY_MEANS_LOG = File.join(File.dirname(__FILE__), "stubs", "kills_by_means_log.log")
+
 Dir.glob(File.join(File.expand_path("..", __dir__), "src", "*.rb")).each do |file|
   autoload File.basename(file, ".rb").camelize, file
 end
